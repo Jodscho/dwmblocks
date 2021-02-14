f62bd8d081b4c16a89fea73982299ad9cf62e4ba
Jodscho <Jonathan2008@gmx.de>
2021-02-14 15:59:01 +0100

    signal support

--- blocks.def.h
+++ blocks.def.h
@@ -1,9 +1,11 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-	{"Mem:", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",	30,		0},
-
-	{"", "date '+%b %d (%a) %I:%M%p'",					5,		0},
+	/*{"Mem:", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",	30,		0},*/
+    {"", "status_weather.sh", 1800, 0},
+    {"", "status_volume.sh", 0, 17},
+    {"", "status_battery.sh", 350, 0},
+	{"", "status_date.sh", 60, 0},
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
