d014bcde5b88b2480133e37d9d17853cb80a7b52
Jodscho <Jonathan2008@gmx.de>
2021-03-28 22:54:44 +0200

    changes

--- blocks.def.h
+++ blocks.def.h
@@ -2,7 +2,7 @@
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
 	/*{"Mem:", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",	30,		0},*/
-    //{"", "status_usb.sh", 0, 18},
+    {"", "status_usb.sh", 0, 18},
     {"", "status_weather.sh", 1800, 0},
     {"", "status_pg.sh", 0, 16},
     {"", "status_volume.sh", 0, 17},
@@ -13,5 +13,5 @@ static const Block blocks[] = {
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
-static char delim[] = " | ";
+static char delim[] = "  ";
 static unsigned int delimLen = 5;
