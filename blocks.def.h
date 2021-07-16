ce747d600928bd3a90980a2e908198b485834e0d
Jodscho <Jonathan2008@gmx.de>
2021-07-16 13:25:25 +0200

    bug fix

--- blocks.def.h
+++ blocks.def.h
@@ -2,14 +2,16 @@
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
 	/*{"Mem:", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",	30,		0},*/
+    {"", "status_music.sh", 1, 20},
+    //{"", "status_wifi.sh", 0, 21},
     {"", "status_usb.sh", 0, 18},
-    {"", "status_weather.sh", 1800, 0},
+    {"", "status_weather.sh", 1800, 19},
     {"", "status_pg.sh", 0, 16},
     {"", "status_volume.sh", 0, 17},
     #ifdef LAPTOP
     {"", "status_battery.sh", 350, 0},
     #endif
-	{"", "status_date.sh", 60, 0},
+    {"", "status_date.sh", 60, 0},
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
